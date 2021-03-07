eb66c3c7517f393c164265c2f6673a1693d39058
Geoffrey Swisher <geoffreydswisher@gmail.com>
2021-03-07 14:41:11 -0500

    janky text scrolling

--- scripts/intro-cutscene/Text-Scroll.gd
+++ scripts/intro-cutscene/Text-Scroll.gd
@@ -4,11 +4,39 @@ extends Control
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var counter = 0
+var prev = -1
+var nodes = 0
+var delay = 10
 
+var default_separation = 1
+var separation = default_separation
+
+var container
+var top
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print('Bruh')
+	container = get_node("VBoxContainer")
+	top = get_node("VBoxContainer/Text")
 
+func _process(_delta):
+	counter += 1
+	nodes = counter / delay
+	_add_node()
 
-
+func _add_node():
+	
+	if nodes != prev and nodes < 50:
+		container.add_constant_override("separation", default_separation)
+		separation = default_separation
+		
+		var new = Label.new()
+		new.text = str(nodes)
+		
+		container.add_child_below_node(top, new)
+	else:
+		separation += 1
+		container.add_constant_override("separation", separation)
+		
+	prev = nodes
