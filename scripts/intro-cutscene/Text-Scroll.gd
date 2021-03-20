786a0d1412e9011964303e9031cf4462f5488444
Geoffrey Swisher <geoffreydswisher@gmail.com>
2021-03-20 16:42:00 -0400

    import oil planet tiles

--- scripts/intro-cutscene/Text-Scroll.gd
+++ scripts/intro-cutscene/Text-Scroll.gd
@@ -4,20 +4,26 @@ extends Control
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var text = ['Today I went to the store.' , 'I got green beans, salsa, and tomatoes.', 'My name? Joseph Madre.']
+
 var counter = 0
 var prev = -1
 var nodes = 0
-var delay = 10
+var delay = 200
 
 var default_separation = 1
 var separation = default_separation
+var sep_inc = .07
 
 var container
+var subcontainer
 var top
+var prev_child
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	container = get_node("VBoxContainer")
+	subcontainer = get_node("VBoxContainer/VBoxContainer")
 	top = get_node("VBoxContainer/Text")
 
 func _process(_delta):
@@ -27,16 +33,18 @@ func _process(_delta):
 
 func _add_node():
 	
-	if nodes != prev and nodes < 50:
+	if nodes != prev and nodes < len(text):
 		container.add_constant_override("separation", default_separation)
 		separation = default_separation
 		
 		var new = Label.new()
-		new.text = str(nodes)
+		new.text = text[nodes]
 		
-		container.add_child_below_node(top, new)
+		subcontainer.add_child(new)
+		subcontainer.move_child(new, 0)
+
 	else:
-		separation += 1
+		separation += sep_inc
 		container.add_constant_override("separation", separation)
 		
 	prev = nodes
