05d2d643bff21c6d7ad6af20ec2033cb6c5326fd
Geoffrey Swisher <geoffreydswisher@gmail.com>
2021-03-25 21:34:22 -0400

    garbage

--- scenes/planet1/enemy.gd
+++ scenes/planet1/enemy.gd
@@ -1,22 +1,34 @@
 extends KinematicBody2D
 
-var gravity = 27
-var friction = 15
 var pos = Vector2(position.x, position.y)
 var velocity = Vector2(0,-10)
-var jumping = false
-var on_floor = false
+var gravity = 27
+var friction = 27
+var jump_strength = 500
+var on_floor = true
+var speed = 30
 
-var jump_strength = -900
-var speed = 300
+var delta = 50
+
+var loc_vector = Vector2.ZERO
+
+var player = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+
+	# use velocity
+	velocity = move_and_slide(velocity, Vector2(0, -1))
 	pass # Replace with function body.
 
+func get_world_objects(object):
+	loc_vector = player.position - global_position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	
+	print(owner.get_node("Player").position)
+	print(position)
 	
 	# use velocity
 	velocity = move_and_slide(velocity, Vector2(0, -1))
@@ -30,30 +42,31 @@ func _process(_delta):
 		if velocity.x > -friction:
 			velocity.x = 0
 		velocity.x = velocity.x + friction
-	
-	
 
 	# gravity calculation
 	velocity.y += gravity
+	
+	if player != null:
+		pass
+	else:
+		print('nope')
 
 	# collision detection
 	for i in get_slide_count():
 		if get_slide_collision(i).collider.name == "Fuel":
 			emit_signal("collided", get_slide_collision(i).collider)
 
-	
-	# input detection
-	check_input()
-
 func jump():
 	if is_on_floor():
 		velocity.y = jump_strength
 		on_floor = false
 
-func check_input():
-	if Input.is_action_pressed("plat_jump"):
-		jump()
-	if Input.is_action_pressed("plat_left"):
-		velocity.x = -speed
-	if Input.is_action_pressed("plat_right"):
-		velocity.x = speed
+func _on_Area2D_body_entered(body):
+	if body != self and body.is_class('KinematicBody2D'):
+		player = body
+	else:
+		player = null
+
+
+func _on_Area2D_body_shape_exited(body_id, body, body_shape, area_shape):
+	player = null
