580b2f936c8c3d5caf4371fe2e6c2bae48b426b5
Michael Hoefler <hoefler2002@gmail.com>
2021-03-26 07:37:13 -0400

    added first enemy

---
+++ scenes/planet1/enemy.gd
@@ -0,0 +1,53 @@
+extends KinematicBody2D
+
+# enemy #1: flies over player and tries to crush them
+
+var velocity = Vector2(0,-10)
+
+var crush_strength = 500
+var speed = 800
+
+var move = Vector2.ZERO
+
+var player = null
+
+func _ready():
+	pass # Replace with function body.
+
+func _process(_delta):
+	
+	# use velocity
+	velocity = move_and_slide(velocity, Vector2(0, -1))
+
+	# collision detection
+	for i in get_slide_count():
+		if get_slide_collision(i).collider.name == "Fuel":
+			emit_signal("collided", get_slide_collision(i).collider)
+
+func rand_crush():
+	var t = rand_range(0, 0.1)
+	yield(get_tree().create_timer(t), 'timeout')
+	velocity.y = crush_strength
+	print('crushed')
+
+func _physics_process(delta):
+	
+	if player != null:
+		move = position.direction_to(player.position) * speed
+		if (move.y > 0): move.y = 0
+		if player.position.y > position.y + 100 and abs(player.position.x - position.x) < 2:
+			rand_crush()
+	else:
+		move = Vector2.ZERO
+	
+	if position.y > 175: move.y -= 1000
+	
+	move = move.normalized()
+	move = move_and_collide(move)
+
+func _on_Area2D_body_entered(body):
+	if body != self and body.is_class('KinematicBody2D'):
+		player = body
+	else:
+		player = null
+
--- scripts/main-menu/Event-Controller.gd
+++ scripts/main-menu/Event-Controller.gd
@@ -1,16 +1,10 @@
 extends MarginContainer
 
 func _ready():
-	save_data(0,0)
+	
 	# connecting button presses to corresponding functions
 	get_node("HBoxContainer/VBoxContainer/Play-Button").connect("pressed", self, "_play_button_pressed")
 
 
 func _play_button_pressed():
 	get_tree().change_scene("res://scenes/intro-cutscene/Main.tscn")
-
-
-func save_data(fuel, parts):
-	var f = File.new()
-	f.open('user://data.save', File.WRITE)
-	f.store_string('%02d\n%02d' % [fuel, parts])
